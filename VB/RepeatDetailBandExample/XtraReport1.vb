9b898742b749b1eeb316053e9c42026207473086
Andrey Lepikhov (auto) <andrey.lepikhov@devexpress.com>
2023-07-11 17:13:00 +0400

    auto sync for version 2023.1

--- VB/RepeatDetailBandExample/XtraReport1.vb
+++ VB/RepeatDetailBandExample/XtraReport1.vb
@@ -13,14 +13,14 @@ Namespace RepeatDetailBandExample
 			InitializeComponent()
 		End Sub
 
-		Private Sub xrLabel5_BeforePrint(ByVal sender As Object, ByVal e As System.Drawing.Printing.PrintEventArgs) Handles xrLabel5.BeforePrint
+		Private Sub xrLabel5_BeforePrint(ByVal sender As Object, ByVal e As System.ComponentModel.CancelEventArgs) Handles xrLabel5.BeforePrint
 			DirectCast(sender, XRLabel).Text &= currentDetail.ToString()
 			DirectCast(sender, XRLabel).Text &= "/"
 			DirectCast(sender, XRLabel).Text &= DetailReport.ReportPrintOptions.DetailCountOnEmptyDataSource.ToString()
 			currentDetail += 1
 		End Sub
 
-		Private Sub Detail_BeforePrint(ByVal sender As Object, ByVal e As System.Drawing.Printing.PrintEventArgs) Handles Detail.BeforePrint
+		Private Sub Detail_BeforePrint(ByVal sender As Object, ByVal e As System.ComponentModel.CancelEventArgs) Handles Detail.BeforePrint
 			currentDetail = 1
 			DetailReport.ReportPrintOptions.DetailCountOnEmptyDataSource = Convert.ToInt32(GetCurrentColumnValue("CategoryID"))
 		End Sub
